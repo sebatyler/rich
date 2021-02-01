206a2e727a59c018e1545926ac7e65576386fd68
sebatyler <sebatyler@gmail.com>
2021-02-01 21:12:46 +0900

    Add mailer

---
+++ test/mailers/coinone_mailer_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class CoinoneMailerTest < ActionMailer::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
---
+++ test/mailers/previews/coinone_mailer_preview.rb
@@ -0,0 +1,4 @@
+# Preview all emails at http://localhost:3000/rails/mailers/coinone_mailer
+class CoinoneMailerPreview < ActionMailer::Preview
+
+end
